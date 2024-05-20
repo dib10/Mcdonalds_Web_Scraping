102713f2345fdb52d4acf7430311c6c2b7e1f0c8
dib10 <caiodib10@gmail.com>
2024-05-20 12:30:45 -0300

    Primeira versão do web scraping do cardápio do mcdondals, contendo categoria, produto e calorias

---
+++ Mcdonalds/urls.py
@@ -0,0 +1,13 @@
+categorias_urls = {
+    'Sanduíches de Carne Bovina': 'https://www.mcdonalds.com.br/cardapio/sanduiches-de-carne-bovina/cheeseburger',
+    'Sanduíches de Frango': 'https://www.mcdonalds.com.br/cardapio/sanduiches-de-frango/mc-crispy-chicken-melt-and-bacon',
+    'Família Tasty': 'https://www.mcdonalds.com.br/cardapio/familia-tasty/big-tasty-turbo-queijo',
+    'Brabos do Méqui': 'https://www.mcdonalds.com.br/cardapio/brabos-do-mequi/brabo-melt-crispy',
+    'Acompanhamentos': 'https://www.mcdonalds.com.br/cardapio/acompanhamentos/chicken-mc-nuggets-10-unidades',
+    'Méqui 1000': 'https://www.mcdonalds.com.br/cardapio/mequi-1000/super-mc',
+    'Café da Manhã': 'https://www.mcdonalds.com.br/cardapio/cafe-da-manha/pao-de-queijo',
+    'McCafé': 'https://www.mcdonalds.com.br/cardapio/mc-cafe/pao-de-queijo',
+    'Bebidas Quentes': 'https://www.mcdonalds.com.br/cardapio/bebidas-quentes/cafe-premium-100ml',
+    'Bebidas Frias': 'https://www.mcdonalds.com.br/cardapio/bebidas-frias/sprite-x-tangerina-e-morango-300-ml',
+    'Sobremesas': 'https://www.mcdonalds.com.br/cardapio/sobremesas/petit-suisse-do-mequi'
+}
