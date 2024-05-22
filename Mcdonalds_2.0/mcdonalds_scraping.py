1207898d97b791d54a73a9d65468fec9e6af2e10
dib10 <caiodib10@gmail.com>
2024-05-22 12:54:21 -0300

    Scipt de percorrer cada produto quando selecionado uma categoria completo, ele clica e volta para a categoria e vai pro proximo produto

--- Mcdonalds_2.0/mcdonalds_scraping.py
+++ Mcdonalds_2.0/mcdonalds_scraping.py
@@ -15,7 +15,9 @@ navegador = webdriver.Chrome(options=options)
 
 sleep(1)
 
-navegador.get('https://www.mcdonalds.com.br/cardapio')
+navegador.get('https://www.mcdonalds.com.br/cardapio/lancamentos')
+
+######## Navegando e clicando nas categorias do cardápio, obs: as categorias ficam num menu lateral esquerdo e não some ########
 
 # Verifique se a classe 'mcd-category-menu' existe na página
 cardapio = navegador.find_element(By.CLASS_NAME, 'mcd-category-menu')
@@ -24,11 +26,43 @@ categorias = cardapio.find_elements(By.CSS_SELECTOR, '.column.is-narrow-mobile.i
 
 ignorar_categoria = ['McLanche Feliz', 'McOferta', 'Méqui Box'] #adicione aqui as categorias que você quer ignorar
 
-for categoria in categorias:
-    if categoria.text in ignorar_categoria:
-        continue
-    categoria.click()
+# for categoria in categorias:
+#     if categoria.text in ignorar_categoria:
+#         continue
+#     categoria.click()
+#     sleep(2)
+
+### Após clicar nas categorias, devemos percorrer cada item e clicar nele ######
+# Verifique se a classe 'columns is-mobile is-multiline is-centered is-gapless' existe na página
+
+div_dos_produtos = navegador.find_element(By.CLASS_NAME, 'columns.is-mobile.is-multiline.is-centered.is-gapless')
+
+# Obter a lista de produtos
+produto_individual = navegador.find_elements(By.CSS_SELECTOR, '.column.is-narrow a')
+
+# Clicar em cada produto
+for i in range(len(produto_individual)):
+    # Reobter a lista de produtos
+    produto_individual = navegador.find_elements(By.CSS_SELECTOR, '.column.is-narrow a')
+    # Clicar no produto
+    produto_individual[i].click()
     sleep(2)
+    # Voltar para a página anterior
+    navegador.back()
+    sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
